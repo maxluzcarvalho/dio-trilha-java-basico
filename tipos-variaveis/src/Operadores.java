7b83775e27bf124241cb8c0b7ad3e096b4436417
Max Luz Carvalho <128647587+maxluzcarvalho@users.noreply.github.com>
2023-04-17 11:06:03 +0200

    atualizacao

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -7,5 +7,27 @@ public class Operadores {
        System.out.println(numero);
        numero = numero * -1;
        System.out.println(numero);
+
+       // public static void main(String[] args){
+       // boolean variavel = true;
+       //variavel =!variavel;
+       //System.out.println(variavel);
+    // }
+    
+    //public static void main(String[] args) {
+       // int a, b;
+        //a = 6;
+        //b = 6;
+        //String resultado = a == b ? "verdadeiro" : "falso";
+        //System.out.println(resultado);
+    //}
+
+    //public static void main(String[] args) {
+       // int nuemro1 = 1;
+       //int numero2 = 2;
+
+       //boolean simNao = numero1 == numero2;
+       //System.out.println("numeroUm é igual a numeroDois?"+ simNao);
+
     }
 }
